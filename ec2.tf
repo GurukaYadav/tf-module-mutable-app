39d019ba07e1aa23d1d9efec1948dc0735246682
Avinash Yadav <avinash.guruka@gmail.com>
2023-03-01 21:29:15 +0530

    updating tf-module-mutable-app

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ resource "aws_spot_instance_request" "instance" {
   instance_type = var.INSTANCE_TYPE
   wait_for_fulfillment = true
   vpc_security_group_ids =  [aws_security_group.sg.id]
-  subnet_id = var.PRIVATE_SUBNET_ID
+  subnet_id = var.PRIVATE_SUBNET_ID[0]
   iam_instance_profile = aws_iam_instance_profile.profile.name
 
   tags = {
