58851aee822967cd3337ffe421244a22c1a261c5
Avinash Yadav <avinash.guruka@gmail.com>
2023-03-04 13:08:46 +0530

    updating tf-module-mutable-app

--- listner.tf
+++ listner.tf
@@ -1,17 +1,16 @@
-#//adding the frontend end instance target grp to public lb
-#resource "aws_lb_listener" "front_end" {
-#  count = var.LB_TYPE == "public" ? 1 : 0
-#  load_balancer_arn = var.LB_ARN
-#  port              = "443"
-#  protocol          = "HTTPS"
-#  ssl_policy        = "ELBSecurityPolicy-2016-08"
-#  certificate_arn   = "arn:aws:acm:us-east-1:124374336606:certificate/534f69be-bcc8-444e-9eb3-973a2f0d10d3"
-#
-#  default_action {
-#    type             = "forward"
-#    target_group_arn = aws_lb_target_group.target-grp.arn
-#  }
-#}
+//adding the frontend end instance target grp to public lb
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = var.LB_ARN
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:acm:us-east-1:124374336606:certificate/534f69be-bcc8-444e-9eb3-973a2f0d10d3"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target-grp.arn
+  }
+}
 
 
 //adding the backend instances target grp to private lb
