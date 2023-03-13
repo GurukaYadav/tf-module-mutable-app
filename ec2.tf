1702435b0514d5a638bfec48589b1e84495cc9b4
Avinash Yadav <avinash.guruka@gmail.com>
2023-03-13 16:30:04 +0530

    updating tf-module-mutable-app

--- ec2.tf
+++ ec2.tf
@@ -31,12 +31,16 @@ resource "aws_ec2_tag" "tag_name" {
 resource "null_resource" "null" {
   count = var.INSTANCE_COUNT
   provisioner "remote-exec" {
+    triggers = {
+      a = timestamp()
+    }
     connection {
       type     = "ssh"
       user     = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["SSH_USER"]
       password = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["SSH_PASS"]
       host     = aws_spot_instance_request.instance.*.private_ip[count.index]
     }
+
     inline = [
       "ansible-pull -U https://github.com/GurukaYadav/roboshop-ansible.git roboshop.yml -e HOST=localhost -e ROLE=${var.COMPONENT} -e ENV=${var.ENV} -e DOCDB_ENDPOINT=${var.DOCDB_ENDPOINT} -e REDIS_ENDPOINT=${var.REDIS_ENDPOINT} -e RDS_ENDPOINT=${var.RDS_ENDPOINT}"
     ]
